b8b69616c3c55fca8ba4e0f2847277655acd8eb4
Cat Flynn <mewtwo97@hotmail.com>
2018-12-29 16:06:36 +0000

    add battery to lemonbar, kill process with x server

--- .config/lemonbar/bar.sh
+++ .config/lemonbar/bar.sh
@@ -4,4 +4,11 @@ font="Misc Tamsyn"
 bar=$(which lemonbar)
 config="$HOME/.config/lemonbar/config.sh"
 
+# terminate already running bar instances
+killall -q $bar
+
+# wait until the processes have been shut down
+while pgrep -u $UID -x $bar >/dev/null; do sleep 1; done
+
+# launch bar
 . $config | $bar -p -f "$font"
--- .config/lemonbar/config.sh
+++ .config/lemonbar/config.sh
@@ -2,17 +2,26 @@
 
 # TODO:
 #
-# battery
 # colours
 # workspace 
 # current window 
 # better date format
+
+battery() {
+  echo -n $(acpi -b) | cut -d, -f3 | awk '{$1=$1;print}' | gawk -F ":" '{print$1":"$2}'
+}
 
 clock() {
   echo -n $(date)
 }
 
 while true; do
-  echo "%{r}$(clock)"
+  # kill process once x is no longer running
+  if ! pgrep -x "Xorg" > /dev/null 
+  then
+    break
+  fi
+
+  echo -n "%{r}$(battery) $(clock)"
   sleep 1;
 done
