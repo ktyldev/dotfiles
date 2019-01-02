a2aefbdc239a0cdd68aba50043fd5119240a2878
Cat Flynn <mewtwo97@hotmail.com>
2019-01-02 12:04:15 +0000

    add workspace indicator to bar

--- .config/lemonbar/config.sh
+++ .config/lemonbar/config.sh
@@ -2,15 +2,16 @@
 
 # import colours from pywal
 . "$HOME/.cache/wal/colors.sh"
-fg="$color8"
-bg="$color0"
+foreground="$color8"
+background="$color0"
+update=0.1
 
 set_colours() {
-  echo -n "%{F$fg}%{B$bg}"
+  echo -n "%{F$foreground}%{B$background}"
 }
 
 set_inverse_colours() {
-  echo -n "%{F$bg}%{B$fg}"
+  echo -n "%{F$background}%{B$foreground}"
 }
 
 battery() {
@@ -21,6 +22,23 @@ clock() {
   echo -n $(date +"%d/%m/%y %T")
 }
 
+bspwm() {
+  # get status of workspaces, cut out unnecessary parts at beginning and
+  # end. could be improved by slicing an array.
+  status=$(bspc wm -g | cut -d: -f2-11)
+  
+  IFS=':' read -r -a arr <<< $status 
+
+  for element in "${arr[@]}"
+  do
+    if [[ $element = F* || $element = O* ]]; then
+      # this will only print one char of the workspace name. make it read
+      # to end!
+      echo -n $(set_inverse_colours) "${element:1}" $(set_colours)
+    fi
+  done
+}
+
 while true; do
   # kill process once x is no longer running
   if ! pgrep -x "Xorg" > /dev/null 
@@ -28,6 +46,6 @@ while true; do
     break
   fi
 
-  echo -n "$(set_colours) %{r}$(battery) $(clock) "
-  sleep 1;
+  echo -n "%{l} $(bspwm) %{r}$(battery) $(clock) "
+  sleep $update;
 done
